c63b7a7bfad252de256521262925c6f2f8d35b39
kk <kk@DESKTOP-3FNCJ3N>
2024-05-28 01:41:48 -0700

    Add project files.

---
+++ Quol Sports/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Quol_Sports.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
